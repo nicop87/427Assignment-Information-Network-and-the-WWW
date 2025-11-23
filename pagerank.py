2d261c9cf0d9d72338213ddca99975ce80a577f1
nicop87 <nicolas.piker@icloud.com>
2025-11-23 14:29:13 -0800

    argparse

---
+++ pagerank.py
@@ -0,0 +1,13 @@
+import scrapy 
+import argparse
+
+
+def main(argv= None):
+    # All the arg parser setup to read the 6 difference command line inputs.
+    # Some are optional while others exit the program if left out.
+    parser = argparse.ArgumentParser(description="Read command line inputs.")
+    parser.add_argument('--crawler', type=str, help='')
+    parser.add_argument('--input', type=str, help='')
+    parser.add_argument('--loglogplot',action='store_true', help='')
+    parser.add_argument('--crawler_graph', type=str, help='')
+    parser.add_argument('pargerank_values', type=str, help='')
