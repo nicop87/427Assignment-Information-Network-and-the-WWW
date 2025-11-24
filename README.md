2bf1245015301a6c4305d8d7328abecd0bf42b1a
LehoPan <leo.long.pan@gmail.com>
2025-11-24 04:16:41 -0800

    bloat files cleaned, readme updated for crawler and crawler_graph

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # 427Assignment-Information-Network-and-the-WWW
+By Leo Pan 030025552 and Nicolas Piker 029966545
+
+## Dependencies
+We installed and utilized the python `networkx`, `scrapy`, and `matplotlib` packages.
+
+## Implementations
+For this project we implemented following functions/arguments:
+- `--crawler`
+    - Following this flag you give the crawler.txt in the format of `example_crawler.txt` to crawl webpages in a domain within a given page limit.
+    - This was done using the python `scrapy` package. Running the spider class in a `CrawlerProcess`, it will recursively navigate webpages in a given domain until it is exhausted pages or has hit the page limit given.
+- `--crawler_graph` 
+    - If the `--crawler` flag was set, this flag is required for the program to run. If not it will display a message and exit. You pass in a desired name for a gml file to output the crawl into.
+
+## Running the program
+Either the `--crawler` or the `--input` must be appropriately set for the program to run. and both the `--crawler` and `--crawler_graph` must be set at the same time.
+
+e.g. commands:
+`python .\page_rank.py --crawler crawler.txt --crawler_graph graph.gml --loglogplot --pagerank_values output.txt`
+
+`python .\page_rank.py --input graph.gml --loglogplot --pagerank_values output.txt`
--- page_rank.py
+++ page_rank.py
@@ -1,13 +1,75 @@
 import scrapy 
 import argparse
 import networkx as nx
-import numpy as np
 import matplotlib.pyplot as plt
-from collections import Counter
+import logging
 
+from collections import Counter
+from urllib.parse import urljoin
+from scrapy.crawler import CrawlerProcess
+from scrapy import signals
+
+# Spider class for the web crawl with scrapy
+class LinkSpider(scrapy.Spider):
+    name = "crawl_links"
+    visited = set() # this will track which pages have already been visited
+    page_count = 0  # keeps track of how many pages we have crawled through
+
+    def __init__(self, urls=None, domain=None, page_limit=None, output=None, **kwargs):
+        super().__init__(**kwargs)
+        self.start_urls = urls
+        self.page_limit = page_limit
+        self.output = output
+
+        self.edges = []
+        self.allowed_domain = domain
+
+    # This and the following spider_closed() functions are to write to a GML file once the crawl is complete
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super().from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+        return spider
+    
+    def spider_closed(self, spider):
+        if self.output != None:
+            mygraph = nx.DiGraph()  # Directed graph
+            mygraph.add_edges_from(self.edges)
+            nx.write_gml(mygraph, self.output)
+
+    # The main function that recursively calls itself to crawl the web page
+    def parse(self, response):
+        content_type = response.headers.get("Content-Type", b"").decode()
+
+        # Only handle HTML pages
+        if "text/html" not in content_type:
+            return  # skip
+        self.visited.add(response.url)
+
+        # Extract links
+        for href in response.css("a::attr(href)").getall():
+            if self.page_count >= self.page_limit:
+                break
+            
+            # This double checks that the link stays within the allowed domain
+            url = urljoin(response.url, href)
+            if self.allowed_domain in url:
+                self.edges.append((response.url, url))
+
+                # Prevent revisiting
+                if url not in self.visited:
+                    self.page_count += 1
+                    yield scrapy.Request(url, callback=self.parse)
+
+        yield {
+            "page": response.url,
+        }
 
 
 def main(argv= None):
+    # Turns off a bunch of font logging messages that bloat the console
+    logging.getLogger('matplotlib.font_manager').setLevel(logging.ERROR)
+
     # All the arg parser setup to read the 6 difference command line inputs.
     # Some are optional while others exit the program if left out.
     parser = argparse.ArgumentParser(description="Read command line inputs.")
@@ -23,9 +85,37 @@ def main(argv= None):
         print("Invalid argument/parameter, please try again.")
         exit()
 
+    # Crawl flag is checked
+    if args.crawler:
+        try:
+            # reads the formatted txt file to crawl
+            with open(args.crawler, 'r') as file:
+                limit = int(file.readline().strip("\n"))    # extracts the page limit
+                myDomain = file.readline().strip("\n")      # extracts the allowed domain
+                myUrls = []
+                for line in file:                           # then extracts all the starting urls
+                    myUrls.append(line.strip("\n"))
+        except FileNotFoundError:
+            print(f"Error: The file '{args.crawler}' was not found.")
+
+        process = CrawlerProcess()
+        # Makes sure the user also gave an output file for the crawl, ends program if not
+        if args.crawler_graph:
+            process.crawl(LinkSpider, urls=myUrls,
+                                    domain=myDomain,
+                                    page_limit=limit,
+                                    output=args.crawler_graph)
+        else:
+            print("Please set the --crawler_graph flag to set an output file for the web crawl. Exiting...")
+            exit()
+        process.start()
+
     G = nx.DiGraph()
-    if args.input:
+    if args.crawler_graph:
+        G = nx.read_gml(args.crawler_graph)
+    elif args.input:
         G = nx.read_gml(args.input)
+    
     if args.loglogplot:
 
         degrees = [d for _, d in G.in_degree()]
@@ -46,8 +136,6 @@ def main(argv= None):
         plt.title("LogLog Plot")
         plt.show()
 
-    if args.crawler_graph:
-        nx.write_gml(G, f"{args.crawler_graph}")
     if args.pagerank_values:
         ranks = nx.pagerank(G)
         sorted_ranks = sorted(ranks.items(), key=lambda x: x[1], reverse=True)
@@ -58,4 +146,5 @@ def main(argv= None):
                 label = G.nodes[node].get("label", str(node))
                 f.write(f"{label}\t{score:.6f}\n")
 
-main()
+if __name__ == "__main__":
+    main()
